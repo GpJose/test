0d4c8b4de6fef9e8464e0e3aece4930898c120f0
GpJose <asdasdg325hbs@gmail.com>
2024-01-06 02:17:35 +0300

    added connectionManager

--- src/main/java/com/example/testproj/config/ConnectionManager.java
+++ src/main/java/com/example/testproj/config/ConnectionManager.java
@@ -1,10 +1,12 @@
 package com.example.testproj.config;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.context.annotation.Configuration;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
+@Configuration
 public class ConnectionManager {
     public static final String serverTimeZone = "Europe/Moscow";
     public static final int portNumber = 3306;
@@ -17,6 +19,8 @@ public class ConnectionManager {
         dataSource.setServerTimezone( serverTimeZone );
         dataSource.setPortNumber( portNumber );
         dataSource.setAllowPublicKeyRetrieval(true);
+        dataSource.setUser("root");
+        dataSource.setPassword("rootROOT1");
 
         return dataSource.getConnection();
     }
